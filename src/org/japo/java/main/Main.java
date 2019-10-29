999b0648a79b3a45e0689efa41610393b2ff79d3
AdriGV <adrian.gimeno.vigo@gmail.com>
2019-10-29 20:42:10 +0100

    :tada: Git initial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,58 @@
+/* 
+ * Copyright 2019 AdrianGV  adrian.gimeno.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+
+import java.util.Random;
+
+/**
+ *
+ * @author AdrianGV adrian.gimeno.alum@iescamp.es
+ */
+public final class Main {
+
+    public static final Random RND = new Random();
+
+
+    public static void main(String[] args) {
+        
+        //Constantes
+        int NUMERO_MIN = 0;
+        int NUMERO_MAX = 9;        
+        int CUENTA_FIN = 6;
+        
+        //Variables
+        int numero = 1;
+        int numeroRND;
+               
+        //Random
+        numeroRND = RND.nextInt(NUMERO_MAX - NUMERO_MIN + 1) + NUMERO_MIN;
+      
+    while ( numero <= CUENTA_FIN){
+        
+            System.out.printf("Número %d ...: %d%n",numero, numeroRND);
+            
+            if (numeroRND == numeroRND) {
+                numeroRND = RND.nextInt(NUMERO_MAX - NUMERO_MIN + 1) + NUMERO_MIN;
+            }
+        
+        //Actualización
+        numero++;
+    }
+    
+    }
+    
+}
